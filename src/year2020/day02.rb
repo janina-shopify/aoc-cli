8fa2f1f0eacb04fbe24a77f44b5d7669ba800699
Janina Szkut <janina.szkut@shopify.com>
2020-12-02 17:47:22 -0500

    Day 2

--- src/year2020/day02.rb
+++ src/year2020/day02.rb
@@ -1,11 +1,17 @@
 module Year2020
   class Day02
     def part1(input)
-      "expected_result"
+      input.split("\n").inject(0) do |num_valid, line|
+        lower, upper, letter, password = line.match(/(\d+)-(\d+)\s([a-z]):\s([a-z]+)/).captures
+        (lower.to_i..upper.to_i).include?(password.count(letter)) ? num_valid + 1 : num_valid
+      end
     end
 
     def part2(input)
-      nil
+      input.split("\n").inject(0) do |num_valid, line|
+        pos1, pos2, letter, password = line.match(/(\d+)-(\d+)\s([a-z]):\s([a-z]+)/).captures
+        (password[pos1.to_i-1] == letter) ^ (password[pos2.to_i-1] == letter) ? num_valid + 1 : num_valid
+      end
     end
   end
 end
